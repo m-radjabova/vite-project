b285f1e28e417260713d3a4945cdb2616b9210a2
m-radjabova <@muslimarajabova1997@gmail.com>
2025-03-17 10:30:40 +0500

    todoForm qo'shildi

--- src/component/TodoForm.tsx
+++ src/component/TodoForm.tsx
@@ -1,10 +1,22 @@
+import { FormEvent, useState } from "react";
 
-function TodoForm() {
+interface Props {
+    addTodo (title: string) : void
+}
+
+function TodoForm(props : Props) {
+    const[title, setTitle] = useState('')
+
+    function add(e: FormEvent){
+        e.preventDefault()
+        props.addTodo(title)
+        setTitle('')
+    }
   return (
     <div>
-        <form>
-            <input type="text" placeholder="Add todo" />
-            <button>Add</button>
+        <form onSubmit={add}>
+            <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Add todo" />
+            <button>Add todo</button>
         </form>
     </div>
   )
