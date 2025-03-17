fd16ae0c746d7d50f882ab972481ef97cde08c76
m-radjabova <@muslimarajabova1997@gmail.com>
2025-03-17 10:21:32 +0500

    vite project

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import TodoList from './component/TodoList';
+import TodoForm from './component/TodoForm';
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Learn React',
+      completed: false
+    },
+    {
+      id: 2,
+      title: 'Learn Vue',
+      completed: false
+    }
+  ])
+
+  function addTodo(title: string) {
+    todos.push({
+      id: todos.length+1,
+      title,
+      completed: false
+    })
+    setTodos([...todos])
+  }
+
+  function setChecked(id: number) {
+    const newTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+
+  return (
+    <div className='container'>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList setChecked={setChecked} todos={todos}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/TodoForm.tsx
@@ -0,0 +1,13 @@
+
+function TodoForm() {
+  return (
+    <div>
+        <form>
+            <input type="text" placeholder="Add todo" />
+            <button>Add</button>
+        </form>
+    </div>
+  )
+}
+
+export default TodoForm;
