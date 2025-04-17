b1d1d301ac2cdf4eab58b327e823490aeb606c24
m-radjabova <@muslimarajabova1997@gmail.com>
2025-04-17 11:10:49 +0500

    usePhoto qo'shildi

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,61 @@
-interface ColourOption {
-    value: number;
-    label: string;
-    color: string;
-  }
-  
-export const colourOptions: ColourOption[] = [
-
-];
+import { StylesConfig } from "react-select";
+import chroma  from 'chroma-js';
 
 
+export interface AlbumOption {
+  value: number;
+  label: string;
+  color: string;
+}
+
+export const colourStyles: StylesConfig<AlbumOption, true> = {
+  control: (styles) => ({ ...styles, backgroundColor: 'white' }),
+  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: isDisabled
+        ? undefined
+        : isSelected
+        ? data.color
+        : isFocused
+        ? color.alpha(0.1).css()
+        : undefined,
+      color: isDisabled
+        ? '#ccc'
+        : isSelected
+        ? chroma.contrast(color, 'white') > 2
+          ? 'white'
+          : 'black'
+        : data.color,
+      cursor: isDisabled ? 'not-allowed' : 'default',
+      ':active': {
+        ...styles[':active'],
+        backgroundColor: !isDisabled
+          ? isSelected
+            ? data.color
+            : color.alpha(0.3).css()
+          : undefined,
+      },
+    };
+  },
+  multiValue: (styles, { data }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: color.alpha(0.1).css(),
+    };
+  },
+  multiValueLabel: (styles, { data }) => ({
+    ...styles,
+    color: data.color,
+  }),
+  multiValueRemove: (styles, { data }) => ({
+    ...styles,
+    color: data.color,
+    ':hover': {
+      backgroundColor: data.color,
+      color: 'white',
+    },
+  }),
+};
