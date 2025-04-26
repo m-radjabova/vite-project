ef6de8b1b5b02ebea5898708a6614f94dd726b38
m-radjabova <@muslimarajabova1997@gmail.com>
2025-04-26 08:43:58 +0500

    login oxiriga keltirildi

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -50,6 +50,13 @@ function Header() {
     <header id="header">
       <img src={Logo} alt="#" />
       <ul>
+        <li> <a href="#home">Home</a>  </li>
+        <li> <a href="#about">About</a></li>
+        <li> <a href="#services">Services</a></li>
+        <li> <a href="#projects">Projects</a></li>
+        <li> <a href="#feedback">Feedback</a> </li>
+        <li> <a href="#blog">Blog</a> </li>
+        <li> <a href="#contact">Contact</a> </li>
         {!user && (
           <>
             <NavLink to="/login" className="login text-decoration-none" style={{ color: '#BDC2FF'  }}>
@@ -59,13 +66,6 @@ function Header() {
             </NavLink>
           </>
         )} 
-        <li> <a href="#home">Home</a>  </li>
-        <li> <a href="#about">About</a></li>
-        <li> <a href="#services">Services</a></li>
-        <li> <a href="#projects">Projects</a></li>
-        <li> <a href="#feedback">Feedback</a> </li>
-        <li> <a href="#blog">Blog</a> </li>
-        <li> <a href="#contact">Contact</a> </li>
         {user && (
           <li className="profile" style={{ listStyle: 'none' }}>
           <div 
